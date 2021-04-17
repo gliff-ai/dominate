4a3f31b7c9f4dc184d9e8241498dfbfd00086702
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-04-17 12:31:42 +0100

    feat: add sign in WIP

--- src/views/SignIn.tsx
+++ src/views/SignIn.tsx
@@ -1,13 +1,46 @@
 import React, { ReactElement, useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { useAuth } from "@/hooks/use-auth";
 import { useHistory } from "react-router-dom";
 
-import { useAuth } from "@/hooks/use-auth";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-export const SignIn = (): ReactElement => {
+export function SignIn() {
+  const classes = useStyles();
   const auth = useAuth();
   const history = useHistory();
 
   const [loading, setLoading] = useState(false);
+
   const [login, setLogin] = useState({
     name: "",
     password: "",
@@ -22,51 +55,159 @@ export const SignIn = (): ReactElement => {
   };
 
   return (
-    // TODO: Click enter to also login
-    <div>
-      <form>
-        <label htmlFor="name">
-          Name:
-          <input
-            type="text"
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Email Address"
             name="name"
+            autoComplete="email"
+            autoFocus
+            type="text"
             onChange={handleChange}
             value={login.name}
-            id="name"
           />
-        </label>
-      </form>
-
-      <form>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="text"
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
             name="password"
-            onChange={handleChange}
-            value={login.password}
+            label="Password"
+            // type="password"
             id="password"
+            autoComplete="current-password"
+            type="text"
+            value={login.password}
+            onChange={handleChange}
           />
-        </label>
-      </form>
-
-      <button
-        type="button"
-        onClick={() => {
-          setLoading(true);
-          auth
-            .signin(login.name, login.password)
-            .then(() => {
-              setLoading(false);
-              history.push("/");
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        }}
-      >
-        {loading ? "Loading..." : "Sign In"}
-      </button>
-    </div>
+          {/* <FormControlLabel
+            control={
+              <Controller
+                as={Checkbox}
+                control={control}
+                name="remember"
+                color="primary"
+                defaultValue={false}
+              />
+            }
+            label="Remember me"
+          /> */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() => {
+              setLoading(true);
+              auth
+                .signin(login.name, login.password)
+                .then(() => {
+                  setLoading(false);
+                  history.push("/");
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+          >
+            {loading ? "Loading..." : "Sign In"}
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
-};
+}
+
+// export const SignIn = (): ReactElement => {
+//   const auth = useAuth();
+//   const history = useHistory();
+
+//   const [loading, setLoading] = useState(false);
+//   const [login, setLogin] = useState({
+//     name: "",
+//     password: "",
+//   });
+
+//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const { id, value } = event.target;
+//     setLogin((prevState) => ({
+//       ...prevState,
+//       [id]: value,
+//     }));
+//   };
+
+//   return (
+//     // TODO: Click enter to also login
+//     <div>
+//       <form>
+//         <label htmlFor="name">
+//           Name:
+//           <input
+//             type="text"
+//             name="name"
+//             onChange={handleChange}
+//             value={login.name}
+//             id="name"
+//           />
+//         </label>
+//       </form>
+
+//       <form>
+//         <label htmlFor="password">
+//           Password:
+//           <input
+//             type="text"
+//             name="password"
+//             onChange={handleChange}
+//             value={login.password}
+//             id="password"
+//           />
+//         </label>
+//       </form>
+
+//       <button
+//         type="button"
+//         onClick={() => {
+//           setLoading(true);
+//           auth
+//             .signin(login.name, login.password)
+//             .then(() => {
+//               setLoading(false);
+//               history.push("/");
+//             })
+//             .catch((err) => {
+//               console.log(err);
+//             });
+//         }}
+//       >
+//         {loading ? "Loading..." : "Sign In"}
+//       </button>
+//     </div>
+//   );
+// };
