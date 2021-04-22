a35430b65436b236e3f1d4f579eeba5f87249f6e
Chas Nelson <chas@gliff.ai>
2021-04-22 17:34:58 +0100

    feat: make Image interface match that used in SOURCE

--- src/etebase/interfaces.ts
+++ src/etebase/interfaces.ts
@@ -7,10 +7,6 @@ interface Gallery {
   description?: string; // "long description for collection, i.e. project details"
 }
 
-interface Image extends Omit<Gallery, "type"> {
-  type: "gliff.image";
-}
-
 interface Thumbnail {
   uid: string;
   type: "gliff.thumbnail";
@@ -28,16 +24,19 @@ type ImageBitmapBlob = string; // TMP
 type Channels = ImageBitmapBlob[]
 type Slices = Channels[];
 
-interface EtebaseImage {
+interface Image {
   meta: {
     type: "gliff.image";
-    slices: number;
-    channels: number;
-    createdTime: number;
-    modifiedTime: number;
-    width: number;
-    height: number;
+    name: string; // human-readable gallery name, i.e. file name
+    slices: number; // number of z-slices
+    channels: number; // numbers colour channels
+    createdTime: number; // time item was created in milliseconds since epoch
+    modifiedTime: number; // time item was last modified in milliseconds since epoch
+    width: number; // width of each slice
+    height: number; // height of each slice
     format: "WebP"; // Maybe other later, maybe we dont convert PNG etc to this
+    description?: string; // long description for collection, i.e. project details
+    customMeta?: string; // JSON of custom metadata
   };
   content: Slices[]
 }
