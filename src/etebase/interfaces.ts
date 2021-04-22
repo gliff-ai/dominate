8c2f73737bbac7021712bc8dea833f4a140b02f5
Chas Nelson <chas@gliff.ai>
2021-04-22 19:41:32 +0100

    fix: try to solve typing issues

--- src/etebase/interfaces.ts
+++ src/etebase/interfaces.ts
@@ -25,17 +25,18 @@ type Channels = ImageBitmapBlob[]
 type Slices = Channels[];
 
 interface Image {
+  uid: string;
+  type: "gliff.image";
+  name: string; // human-readable gallery name, i.e. file name
+  createdTime: number; // time item was created in milliseconds since epoch
+  modifiedTime: number; // time item was last modified in milliseconds since epoch
+  description?: string; // long description for collection, i.e. project details
   meta: {
-    type: "gliff.image";
-    name: string; // human-readable gallery name, i.e. file name
     slices: number; // number of z-slices
     channels: number; // numbers colour channels
-    createdTime: number; // time item was created in milliseconds since epoch
-    modifiedTime: number; // time item was last modified in milliseconds since epoch
     width: number; // width of each slice
     height: number; // height of each slice
     format: "WebP"; // Maybe other later, maybe we dont convert PNG etc to this
-    description?: string; // long description for collection, i.e. project details
     customMeta?: string; // JSON of custom metadata
   };
   content: Slices[]
