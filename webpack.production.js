c68f6cfa67f94f214fcf8dbf4d2bf0c85a5a76ec
Craig Cooper <c@c9r.dev>
2021-06-04 13:03:43 +0100

    Prod deploy (#87)
    * ci: add Azure Static Web Apps workflow file
    on-behalf-of: @Azure opensource@microsoft.com
    
    * build: prod deploy scripts

---
+++ webpack.production.js
@@ -0,0 +1,18 @@
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs",
+    clean: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      STORE_URL: JSON.stringify("https://store.gliff.app/"),
+    }),
+  ],
+});
