9bc2efe4c864d8e66c39bbc133291d1711778df2
Chas Nelson <github@chasnelson.co.uk>
2021-06-23 09:36:29 +0100

    chore: format

--- src/index.tsx
+++ src/index.tsx
@@ -17,57 +17,57 @@ declare const VERSION: string;
 const version = VERSION;
 
 if (IS_MONITORED) {
-    // setup Sentry
-    Sentry.init({
-        dsn: "https://097ef1f6a3364e6895c2fcb95c88446a@o651808.ingest.sentry.io/5812330",
-        tunnel: `${STORE_URL}api/tunnel/`,
+  // setup Sentry
+  Sentry.init({
+    dsn: "https://097ef1f6a3364e6895c2fcb95c88446a@o651808.ingest.sentry.io/5812330",
+    tunnel: `${STORE_URL}api/tunnel/`,
 
-        integrations: [
-            //   new Integrations.BrowserTracing(),
-            /* eslint-disable @typescript-eslint/no-unsafe-call */
-            new CaptureConsole({
-                // options: ['log', 'info', 'warn', 'error', 'debug', 'assert']
-                levels: ["assert"],
-            }),
-            /* eslint-enable @typescript-eslint/no-unsafe-call */
-        ],
+    integrations: [
+      //   new Integrations.BrowserTracing(),
+      /* eslint-disable @typescript-eslint/no-unsafe-call */
+      new CaptureConsole({
+        // options: ['log', 'info', 'warn', 'error', 'debug', 'assert']
+        levels: ["assert"],
+      }),
+      /* eslint-enable @typescript-eslint/no-unsafe-call */
+    ],
 
-        release: `dominate@${version}`,
+    release: `dominate@${version}`,
 
-        // We recommend adjusting this value in production, or using tracesSampler
-        // for finer control
-        tracesSampleRate: 1.0,
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
 
-        // flag for filtering
-        environment: SENTRY_ENVIRONMENT,
+    // flag for filtering
+    environment: SENTRY_ENVIRONMENT,
 
-        debug: IS_SENTRY_DEBUG,
+    debug: IS_SENTRY_DEBUG,
+  });
+
+  // setup LogRocket
+  setupLogRocketReact(LogRocket);
+  LogRocket.init("ia4fn6/dominate", {
+    dom: {
+      textSanitizer: true,
+      inputSanitizer: true,
+    },
+  });
+
+  // add LogRocket sessions to Sentry issues
+  LogRocket.getSessionURL((sessionURL) => {
+    Sentry.configureScope((scope) => {
+      scope.setExtra("sessionURL", sessionURL);
     });
-
-    // setup LogRocket
-    setupLogRocketReact(LogRocket);
-    LogRocket.init("ia4fn6/dominate", {
-        dom: {
-            textSanitizer: true,
-            inputSanitizer: true,
-        },
-    });
-
-    // add LogRocket sessions to Sentry issues
-    LogRocket.getSessionURL((sessionURL) => {
-        Sentry.configureScope((scope) => {
-            scope.setExtra("sessionURL", sessionURL);
-        });
-    });
+  });
 }
 
 const etebaseInstance = new DominateEtebase();
 
 ReactDOM.render(
-    <Sentry.ErrorBoundary fallback="An error has occurred" showDialog>
-        <ProvideAuth etebaseInstance={etebaseInstance}>
-            <UserInterface etebaseInstance={etebaseInstance} />
-        </ProvideAuth>
-    </Sentry.ErrorBoundary>,
-    document.getElementById("react-container")
+  <Sentry.ErrorBoundary fallback="An error has occurred" showDialog>
+    <ProvideAuth etebaseInstance={etebaseInstance}>
+      <UserInterface etebaseInstance={etebaseInstance} />
+    </ProvideAuth>
+  </Sentry.ErrorBoundary>,
+  document.getElementById("react-container")
 );
