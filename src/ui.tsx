68822ff675427eea6c5f34157d1aad41bf5ecf72
Craig Cooper <c@c9r.dev>
2021-07-05 10:37:46 +0100

    fix: make ui.tsx pure

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,22 +1,9 @@
-/* eslint-disable react/jsx-curly-newline */
-import React, { Component, ReactNode } from "react";
+import React, { Component, ReactElement, ReactNode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {
-  Avatar,
-  Card,
-  CssBaseline,
-  IconButton,
-  Theme,
-  ThemeProvider,
-  Tooltip,
-  Typography,
-  WithStyles,
-  withStyles,
-} from "@material-ui/core";
+import { ThemeProvider, WithStyles, withStyles } from "@material-ui/core";
 import { theme } from "@/theme";
-import SVG from "react-inlinesvg";
 
-import { Collection, DominateEtebase } from "@/etebase";
+import { DominateEtebase } from "@/etebase";
 import { SignIn } from "@/views/SignIn";
 import { SignUp } from "@/views/SignUp";
 import { RecoverAccount } from "@/views/RecoverAccount";
@@ -29,7 +16,6 @@ import { CurateWrapper } from "./CurateWrapper";
 import { RecoveryKey } from "./views/RecoveryKey";
 import { TeamMembers } from "./views/TeamMembers";
 import { Account } from "./views/Account";
-
 import { ResetPassword } from "./views/ResetPassword";
 
 const styles = {
@@ -63,110 +49,76 @@ const styles = {
 
 interface Props extends WithStyles<typeof styles> {
   etebaseInstance: DominateEtebase;
-  // children?: Children;
 }
 
-interface State {
-  collections?: Collection[];
-}
-
-class UserInterface extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { collections: null };
-  }
-
-  render = (): ReactNode => {
-    const { classes } = this.props;
-    return (
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <div className={classes.outerContainer}>
-            <Routes>
-              {window.location.pathname === "/signin" ||
-              window.location.pathname === "/signup" ? (
-                <></>
-              ) : (
-                <Navbar />
-              )}
+const UserInterface = (props: Props): ReactElement | null => {
+  const { classes, etebaseInstance } = props;
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div className={classes.outerContainer}>
+          <Routes>
+            {window.location.pathname === "/signin" ||
+            window.location.pathname === "/signup" ? (
+              <></>
+            ) : (
               <Navbar />
-              <Route path="/signin">
-                <SignIn />
-              </Route>
-              <Route path="/signup">
-                <SignUp />
-              </Route>
-              <Route
-                path="curate/:id"
-                element={
-                  <CurateWrapper etebaseInstance={this.props.etebaseInstance} />
-                }
-              />
-              <Route
-                path="curate/"
-                element={
-                  <CurateWrapper etebaseInstance={this.props.etebaseInstance} />
-                }
-              />
-              <Route
-                path="annotate/:collectionUid/:imageUid"
-                element={
-                  <AnnotateWrapper
-                    etebaseInstance={this.props.etebaseInstance}
-                  />
-                }
-              />
-              <Route
-                path="manage/*"
-                element={
-                  <ManageWrapper etebaseInstance={this.props.etebaseInstance} />
-                }
-              />
-              <Route
-                path="recover/*"
-                element={
-                  <RecoverAccount
-                    etebaseInstance={this.props.etebaseInstance}
-                  />
-                }
-              />
-              <Route
-                path="request-recover/*"
-                element={<RequestRecoverAccount />}
-              />
-              <Route path="/">
-                <Home />
-              </Route>
+            )}
+            <Navbar />
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route
+              path="curate/:id"
+              element={<CurateWrapper etebaseInstance={etebaseInstance} />}
+            />
+            <Route
+              path="curate/"
+              element={<CurateWrapper etebaseInstance={etebaseInstance} />}
+            />
+            <Route
+              path="annotate/:collectionUid/:imageUid"
+              element={<AnnotateWrapper etebaseInstance={etebaseInstance} />}
+            />
+            <Route
+              path="manage/*"
+              element={<ManageWrapper etebaseInstance={etebaseInstance} />}
+            />
+            <Route
+              path="recover/*"
+              element={<RecoverAccount etebaseInstance={etebaseInstance} />}
+            />
+            <Route
+              path="request-recover/*"
+              element={<RequestRecoverAccount />}
+            />
+            <Route path="/">
+              <Home />
+            </Route>
 
-              <Route path="/recoverykey">
-                <RecoveryKey />
-              </Route>
+            <Route path="/recoverykey">
+              <RecoveryKey />
+            </Route>
 
-              <Route path="/teammembers">
-                <TeamMembers />
-              </Route>
+            <Route path="/teammembers">
+              <TeamMembers />
+            </Route>
 
-              <Route path="/resetpassword">
-                <ResetPassword />
-              </Route>
+            <Route path="/reset-password">
+              <ResetPassword />
+            </Route>
 
-              <Route path="/account">
-                <Account />
-              </Route>
-            </Routes>
-          </div>
-
-          <footer>
-            <div>
-              {this.state.collections?.map((col) =>
-                JSON.stringify(col.getMeta())
-              )}
-            </div>
-          </footer>
-        </BrowserRouter>
-      </ThemeProvider>
-    );
-  };
-}
+            <Route path="/account">
+              <Account />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
 
 export default withStyles(styles)(UserInterface);
