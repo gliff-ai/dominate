fbea6209e239186f6f2b5ca372df16ad5a7794ed
Silvia Zeni <silvia.szn@gmail.com>
2021-07-22 17:14:46 +0100

    feat: Navbar with system's user flow (#203)
    * feat: Navbar with system's user flow
    
    * fix: Lint
    
    * fix: Lint

---
+++ src/components/productIcons/BaseProductIcon.tsx
@@ -0,0 +1,100 @@
+import { ReactElement } from "react";
+import { Avatar, makeStyles, Link, Theme } from "@material-ui/core";
+import SVG from "react-inlinesvg";
+import { imgSrc } from "@/theme";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  productSvg: {
+    width: "39px",
+    height: "39px",
+  },
+  productAvatar: {
+    position: "relative",
+    margin: "0 auto",
+  },
+  iconDiv: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "5px",
+    marginLeft: "10px",
+    marginRight: "10px",
+    flexDirection: "column",
+  },
+  productName: {
+    fontWeight: 500,
+    marginTop: "5px",
+    textTransform: "uppercase",
+  },
+  trailSvg: {
+    width: "7px",
+    height: "7px",
+    marginTop: -30,
+    position: "relative",
+    fill: theme.palette.text.secondary,
+  },
+  outerDiv: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+}));
+
+interface Props {
+  tool: string;
+  buttonKey?: string;
+  linkDisabled?: boolean;
+  extraStyleAvatar?: string | null;
+  extraStyleSvg?: string | null;
+  extraStyleName?: string | null;
+  extraStyleTrailSvg?: string | null;
+}
+
+function BaseProductIcon({
+  tool,
+  buttonKey,
+  linkDisabled,
+  extraStyleAvatar,
+  extraStyleSvg,
+  extraStyleName,
+  extraStyleTrailSvg,
+}: Props): ReactElement {
+  const classes = useStyles();
+
+  const avatar = (
+    <Avatar
+      variant="circular"
+      className={`${classes.productAvatar} ${extraStyleAvatar}`}
+    >
+      <SVG
+        src={imgSrc(tool)}
+        className={`${classes.productSvg} ${extraStyleSvg}`}
+      />
+    </Avatar>
+  );
+
+  return (
+    <div key={buttonKey} className={classes.outerDiv}>
+      <div className={classes.iconDiv}>
+        {linkDisabled ? avatar : <Link href={`/${tool}`}>{avatar}</Link>}
+        <p className={`${classes.productName} ${extraStyleName}`}>{tool}</p>
+      </div>
+      {tool !== "annotate" && (
+        <SVG
+          src={imgSrc("breadcrumb-trail")}
+          className={`${classes.trailSvg} ${extraStyleTrailSvg}`}
+        />
+      )}
+    </div>
+  );
+}
+
+BaseProductIcon.defaultProps = {
+  buttonKey: null,
+  linkDisabled: false,
+  extraStyleAvatar: null,
+  extraStyleSvg: null,
+  extraStyleName: null,
+  extraStyleTrailSvg: null,
+};
+
+export { BaseProductIcon };
