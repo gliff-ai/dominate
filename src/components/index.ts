d6715fbf025ce955a2d38aa406252f34deba28ba
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-07-29 17:27:18 +0100

    feat: import @gliff-ai/style from npm instead of tar file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { NavBar } from "./NavBar";
 export { MessageAlert } from "./message/MessageAlert";
 export { MessageSnackbar } from "./message/MessageSnackbar";
-export { BaseSnackbarProps, TransitionProps } from "./BaseSnackbar";
 export { ProgressSnackbar, Task } from "./ProgressSnackbar";
 export { SubmitButton } from "./SubmitButton";
 export { PageSpinner } from "./PageSpinner";
