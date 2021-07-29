950fffe1b9ae3dd95c69f62ee86372823417cc12
Christian Baidoo <37402968+ChrisBaidoo@users.noreply.github.com>
2021-07-29 16:39:22 +0100

    Move out base snack bar (#204)
    * build: prod action
    
    * build: prod action
    
    * chore: use gliff-ai-style-0.0.1.tgz file for theme
    
    * chore: move imgSrc into a separate file
    
    * fix: lint
    
    * chore: import theme from @gliff-ai/style
    
    * chore: remove gliff-ai-style-0.0.1.tgz file
    
    * add @gliff-ai/style
    
    * chore: change variable name from t to theme
    
    * fix: import from appropriate modules
    
    * chore: import BaseSnackBar from @gliff-ai-style
    
    * fix: import BaseSnackBar module WIP
    
    * fix: BaseSnackbar works
    
    * chore: import @gliff-ai/style from npm instead of the tar file
    
    Co-authored-by: Craig Cooper <c@c9r.dev>

--- src/views/SignUp.tsx
+++ src/views/SignUp.tsx
@@ -17,16 +17,11 @@ import {
 } from "@material-ui/core";
 import Slide from "@material-ui/core/Slide";
 import { useNavigate } from "react-router-dom";
-import { theme } from "@gliff-ai/style";
+import { theme, TransitionProps } from "@gliff-ai/style";
 import { useAuth } from "@/hooks/use-auth";
 import { createCheckoutSession, getInvite } from "@/services/user";
 import { RecoveryKey } from "@/views/RecoveryKey";
-import {
-  MessageSnackbar,
-  MessageAlert,
-  TransitionProps,
-  SubmitButton,
-} from "@/components";
+import { MessageSnackbar, MessageAlert, SubmitButton } from "@/components";
 
 const stripePromise = loadStripe(
   "pk_test_51IVYtvFauXVlvS5w0UZBrzMK5jOZStppHYgoCBLXsZjOKkyqLWC9ICe5biwlYcDZ8THoXtOlPXXPX4zptGjJa1J400IAI0fEAo"
