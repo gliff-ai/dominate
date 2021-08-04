e8f7cb54b3e3c8655ac925efb51868236ddf9c2a
Chas Nelson <github@chasnelson.co.uk>
2021-08-04 17:35:19 +0100

    feat: add getCollaboratorProject to MANAGE wrapper services

--- src/wrappers/ManageWrapper.tsx
+++ src/wrappers/ManageWrapper.tsx
@@ -28,6 +28,31 @@ export const ManageWrapper = (props: Props): ReactElement | null => {
     return projects;
   };
 
+  const getCollaboratorProject = async ({ name }) => {
+    console.log("getting projects");
+    const projects = await props.etebaseInstance.getCollectionsMeta();
+
+    // get all members of all team projects
+    const membersPromises = [];
+    for (let p = 0; p < projects.length; p += 1) {
+      const { uid } = projects[p];
+      membersPromises.push(props.etebaseInstance.getCollectionMembers(uid));
+    }
+    const allMembers: string[][] = await Promise.all(membersPromises);
+
+    // for each project, go through members and return the first that matches
+    for (let p = 0; p < projects.length; p += 1) {
+      const members = allMembers[p];
+      for (let m = 0; p < members.length; m += 1) {
+        if (members[m] === name) {
+          return projects[p].uid;
+        }
+      }
+    }
+
+    return null;
+  };
+
   const createProject = async ({ name }) => {
     const project = await props.etebaseInstance.createCollection(name);
 
@@ -69,6 +94,7 @@ export const ManageWrapper = (props: Props): ReactElement | null => {
     loginUser: "POST /user/login", // Not used, we pass an authd user down
     getProjects,
     getProject: "GET /project/", // TODO
+    getCollaboratorProject,
     createProject,
     inviteUser,
     inviteCollaborator,
