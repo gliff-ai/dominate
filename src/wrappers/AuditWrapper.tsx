6b26aee71d708e0fc5bd614b29fd0d15d0dc26c5
phil <philiptgjackson@gmail.com>
2021-08-17 15:20:58 +0100

    feat: add AuditWrapper.tsx

---
+++ src/wrappers/AuditWrapper.tsx
@@ -0,0 +1,14 @@
+import { ReactElement, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { }
+import { DominateEtebase } from "@/etebase";
+
+interface Props {
+  etebaseInstance: DominateEtebase;
+  setIsLoading: (isLoading: boolean) => void;
+}
+
+export const CurateWrapper = (props: Props): ReactElement => {
+  return <></>
+};
