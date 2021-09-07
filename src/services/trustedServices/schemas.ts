3cf60e4e481657444d86cc960301895a41e77e04
Silvia Zeni <silvia.szn@gmail.com>
2021-09-07 09:33:16 +0100

    chore: Make UiTemplateSchema more specific

--- src/services/trustedServices/schemas.ts
+++ src/services/trustedServices/schemas.ts
@@ -9,36 +9,32 @@ const UiTemplateSchema: JSONSchemaType<UiTemplate> = {
     trustedService: { type: "string" },
     uiElements: {
       type: "array",
-      default: [],
+      minItems: 1,
       items: {
-        anyOf: [
-          {
+        type: "object",
+        required: ["apiEndpoint", "uiParams", "placement"],
+        additionalProperties: false,
+        properties: {
+          apiEndpoint: { type: "string" },
+          uiParams: {
             type: "object",
-            required: ["apiEndpoint", "uiParams", "placement"],
+            required: ["icon", "tooltip"],
             additionalProperties: false,
             properties: {
-              apiEndpoint: { type: "string" },
-              uiParams: {
-                type: "object",
-                required: ["icon", "tooltip"],
-                additionalProperties: false,
-                properties: {
-                  tag: { type: "string", nullable: true },
-                  value: { type: "string", nullable: true },
-                  icon: { type: "string" },
-                  tooltip: { type: "string" },
-                },
-              },
-              placement: {
-                type: "array",
-                items: {
-                  type: "string",
-                  enum: ["curate", "annotate"],
-                },
-              },
+              tag: { type: "string", nullable: true },
+              value: { type: "string", nullable: true },
+              icon: { type: "string" },
+              tooltip: { type: "string" },
             },
           },
-        ],
+          placement: {
+            type: "array",
+            items: {
+              type: "string",
+              enum: ["curate", "annotate"],
+            },
+          },
+        },
       },
     },
   },
