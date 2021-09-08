0decb457ca5f94a39320ec2b9cb1aed53cc79000
Craig Cooper <c@c9r.dev>
2021-09-08 08:36:56 +0000

    build: update prod build

---
+++ src/components/CookieConsent.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import ReactCookieConsentDefault from "react-cookie-consent";
+import { theme } from "@gliff-ai/style";
+import { MessageAlert } from "./message/MessageAlert";
+
+function CookieConsent(): JSX.Element {
+  // TODO patch this
+  // https://github.com/Mastermindzh/react-cookie-consent/issues/110
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const ReactCookieConsent = ReactCookieConsentDefault.default || ReactCookieConsentDefault; // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+
+  const [isCookiesConsented, setIsCookiesConsented] = useState("");
+  return (
+    <>
+      <MessageAlert severity="error" message={isCookiesConsented} />
+      <ReactCookieConsent
+        location="bottom"
+        cookieName="gliff-ai-consent-cookie"
+        expires={999}
+        overlay
+        style={{
+          backgroundColor: "#ffffff",
+          color: "#000000",
+        }}
+        buttonStyle={{
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.text.primary,
+          textTransform: "none",
+          fontWeight: 700,
+          fontSize: "15px",
+          width: "169px",
+          marginBottom: "20px",
+          marginTop: "20px",
+          borderRadius: "9px",
+        }}
+        declineButtonStyle={{
+          backgroundColor: theme.palette.secondary.main,
+          color: theme.palette.text.primary,
+          textTransform: "none",
+          fontWeight: 700,
+          fontSize: "15px",
+          width: "169px",
+          marginBottom: "20px",
+          marginTop: "20px",
+          borderRadius: "9px",
+        }}
+        enableDeclineButton
+        setDeclineCookie={false}
+        onDecline={() => {
+          setIsCookiesConsented(
+            "Without cookies this app will not work, redirecting you to our homepage."
+          );
+          setTimeout(() => window.location.replace("https://gliff.ai"), 1500);
+        }}
+      >
+        This website uses cookies to enhance the user experience (we don&apos;t
+        use tracking or advertising cookies).
+      </ReactCookieConsent>
+    </>
+  );
+}
+
+export {CookieConsent};
