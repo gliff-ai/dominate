3c1f0dae834119e9fda62167be6aba6cc6a1cfc8
Silvia Zeni <silvia.szn@gmail.com>
2021-09-21 14:38:55 +0100

    chore: Export usePlugins from hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
 export { ProvideAuth } from "@/hooks/use-auth";
 export { ProvideTrustedService } from "@/hooks/use-trustedService";
-export { ProvidePlugins } from "@/hooks/use-plugin";
+export { ProvidePlugins, usePlugins } from "@/hooks/use-plugin";
