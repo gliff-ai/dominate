385b3c1bf921c9ffbfd66e21ce62b44e792f42d7
Craig Cooper <c@c9r.dev>
2021-09-28 11:06:44 +0100

    fix: ts paths

--- src/services/trustedServices/index.ts
+++ src/services/trustedServices/index.ts
@@ -2,21 +2,21 @@ import { apiRequest } from "@/api";
 import type { TrustedService, UiTemplate } from "./interfaces";
 
 export const getTrustedService = (): Promise<TrustedService[]> =>
-  apiRequest<TrustedService[]>(`trusted_service/`, "GET");
+  apiRequest<TrustedService[]>(`/trusted_service/`, "GET");
 
 export const createTrustedService = (
   id: string,
   name: string,
   base_url: string
 ): Promise<number> =>
-  apiRequest<number>("trusted_service/", "POST", {
+  apiRequest<number>("/trusted_service/", "POST", {
     id,
     name,
     base_url,
   });
 
 export const getUiTemplate = (apiUrl: string): Promise<UiTemplate> =>
-  apiRequest<UiTemplate>("ui-template", "POST", {}, apiUrl);
+  apiRequest<UiTemplate>("/ui-template", "POST", {}, apiUrl);
 
 export { TrustedService };
 export { TrustedServiceClass } from "./TrustedServiceClass";
