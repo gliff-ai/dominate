b555fe174390e1dfb090e549775671583c41a807
Silvia Zeni <silvia.szn@gmail.com>
2021-09-30 11:32:06 +0100

    fix: Update plugins to reflect backend changes

--- src/services/plugins/index.ts
+++ src/services/plugins/index.ts
@@ -1,16 +1,11 @@
 import { apiRequest } from "@/api";
 import type { Plugin, Product } from "./interfaces";
 
-export const getPlugins = (teamId: number): Promise<Plugin[]> =>
-  apiRequest<Plugin[]>(`/plugin/${teamId}`, "GET");
+export const getPlugins = (): Promise<Plugin[]> =>
+  apiRequest<Plugin[]>(`/plugin/`, "GET");
 
-export const createPlugin = (
-  teamId: number,
-  url: string,
-  product: Product
-): Promise<number> =>
+export const createPlugin = (url: string, product: Product): Promise<number> =>
   apiRequest<number>("/plugin/", "POST", {
-    team_id: teamId,
     url,
     product,
   });
