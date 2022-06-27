dd1657ab0cbbbeb04f638fe1260252c3b3da9414
Philip Jackson <philiptgjackson@gmail.com>
2022-06-27 17:36:44 +0100

    feat: add ProjectAudit.ts

---
+++ src/interfaces/ProjectAudit.ts
@@ -0,0 +1,83 @@
+import { BaseMeta } from "./shared";
+
+// ----------------------------- META -----------------------------
+
+interface ProjectAuditMetaV0 extends BaseMeta {
+  meta_version: 0;
+  content_version: 0;
+  type: "gliff.projectAudit";
+  createdTime: number;
+  modifiedTime: number;
+  deletedTime: number;
+  galleryUID: string;
+}
+
+interface UploadImageV0 {
+  type: "uploadImage";
+  imageName: string;
+  imageUid: string;
+}
+
+interface DeleteImageV0 {
+  type: "deleteImage";
+  imageName: string;
+  imageUid: string;
+}
+
+interface AssignImageV0 {
+  type: "assignImage";
+  imageName: string;
+  imageUid: string;
+  inviteeUsername: string;
+}
+
+interface UpdateImageLabelsV0 {
+  type: "updateImageLabels";
+  imageName: string;
+  imageUid: string;
+  labels: string[];
+}
+
+interface InviteUserV0 {
+  type: "inviteUser";
+  inviteeUsername: string;
+}
+
+interface InviteAcceptedV0 {
+  type: "inviteAccepted";
+  inviteeUsername: string;
+}
+
+interface SetDefaultLabelsV0 {
+  type: "setDefaultLabels";
+  defaultLabels: string[];
+}
+
+interface SetRestrictToDefaultLabelsV0 {
+  type: "setRestrictToDefaultLabels";
+  restrict: boolean;
+}
+
+interface SetMultiLabelV0 {
+  type: "setMultiLabel";
+  multiLabel: boolean;
+}
+
+// ----------------------------- CONTENT -----------------------------
+
+interface ProjectAuditActionV0 {
+  action:
+    | UploadImageV0
+    | DeleteImageV0
+    | AssignImageV0
+    | UpdateImageLabelsV0
+    | InviteUserV0
+    | InviteAcceptedV0
+    | SetDefaultLabelsV0
+    | SetRestrictToDefaultLabelsV0
+    | SetMultiLabelV0;
+  username: string;
+  timestamp: number;
+}
+
+type ProjectAuditContentV0 = ProjectAuditActionV0[];
