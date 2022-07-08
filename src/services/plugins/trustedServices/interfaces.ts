276cea0b9d45e6204f211e5bb6443d1a3fec0c29
Silvia Zeni <silvia.szn@gmail.com>
2022-07-08 14:58:09 +0100

    Merge remote-tracking branch 'origin/issue656' into addZoo (part2)

--- src/services/plugins/trustedServices/interfaces.ts
+++ src/services/plugins/trustedServices/interfaces.ts
@@ -8,27 +8,10 @@ interface TrustedService extends JsPlugin {
 
 interface TrustedServiceIn extends TrustedService, JsPluginIn {}
 interface TrustedServiceOut extends TrustedService, JsPluginOut {}
-
 interface UiTemplate {
-  trustedService: string;
-  uiElements: UiElement[];
-}
-
-interface UiElement {
-  placement: string[]; // TODO: delete
-  apiEndpoint: string;
-  uiParams: {
-    tag?: string; // NOTE: unused
-    value?: string; // NOTE: unused
-    icon: string; // TODO: delete
-    tooltip: string;
+  ui: {
+    button: { name?: string; tooltip: string };
   };
 }
 
-export {
-  TrustedService,
-  TrustedServiceIn,
-  TrustedServiceOut,
-  UiTemplate,
-  UiElement,
-};
+export { TrustedService, TrustedServiceIn, TrustedServiceOut, UiTemplate };
